f3d988783ca8f6c6b2f7d0993d5c02f46437c5e1
SumaylaKhan <SumaylaKhan@DESKTOP-UKODEMR>
2019-01-09 16:53:18 +0530

    New project

---
+++ GitHub_Practice/src/main/java/GitHub_Practice/GitHub_Practice/App.java
@@ -0,0 +1,13 @@
+package GitHub_Practice.GitHub_Practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ GitHub_Practice/src/test/java/GitHub_Practice/GitHub_Practice/NewTest.java
@@ -0,0 +1,13 @@
+package GitHub_Practice.GitHub_Practice;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  
+	  System.out.println("a");
+  }
+  
+  
+}
